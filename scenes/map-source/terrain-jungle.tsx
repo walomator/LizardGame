9ec994b218e68504155328a9af22c69d7db0568c
Waluigi <walomator@protonmail.com>
2017-07-06 18:32:10 -0400

    feat: Add a tilemap imported from Tiled into the World scene
    This tilemap only works as a background for now.

--- scenes/map-source/terrain-jungle.tsx
+++ scenes/map-source/terrain-jungle.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="jungle-v1" tilewidth="32" tileheight="32" tilecount="9" columns="3">
+<tileset name="terrain-jungle" tilewidth="32" tileheight="32" tilecount="9" columns="3">
  <image source="terrain-jungle.png" width="96" height="96"/>
 </tileset>
