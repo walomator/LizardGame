9bdf3942d5e868395fd527bfaad67a7e47bf69d9
Waluigi <walomator@protonmail.com>
2017-07-09 21:18:12 -0400

    dev: Replace individual blocks with tilemap

--- scenes/map-source/terrain-jungle.tsx
+++ scenes/map-source/terrain-jungle.tsx
@@ -1,4 +1,44 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="terrain-jungle" tilewidth="32" tileheight="32" tilecount="9" columns="3">
  <image source="terrain-jungle.png" width="96" height="96"/>
+ <tile id="0">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index">
+   <object id="3" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index">
+   <object id="2" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <objectgroup draworder="index">
+   <object id="2" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
 </tileset>
