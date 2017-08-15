19f4ed6166270304518f857d8e29c953fd86d8e2
Waluigi <walomator@protonmail.com>
2017-08-15 18:14:16 -0400

    dev: Use new and improved tilesets for maps

---
+++ scenes/map-source/special.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="special" tilewidth="32" tileheight="32" tilecount="16" columns="4">
+ <image source="../../../../../../../../home/xenon/Documents/godot/LizardGame/scenes/map-source/special.png" width="128" height="128"/>
+</tileset>
---
+++ scenes/map-source/terrain-normal.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="terrain-normal" tilewidth="32" tileheight="32" tilecount="4" columns="2">
+ <image source="../../../../../../../../home/xenon/Documents/godot/LizardGame/scenes/map-source/terrain-normal.png" width="64" height="64"/>
+ <tile id="0">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="4" width="32" height="28"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="4" width="32" height="28"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index">
+   <object id="5" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+</tileset>
