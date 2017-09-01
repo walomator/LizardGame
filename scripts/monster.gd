f1231e6add208960be2ac9f0eae9a96852883640
Waluigi <walomator@protonmail.com>
2017-09-01 16:04:54 -0400

    fix: Correct monster.gd script not being up to date in last commit

--- scripts/monster.gd
+++ scripts/monster.gd
@@ -1,5 +1,16 @@
 extends KinematicBody2D
 
+
+var idle_anim_node
+var collision_box_node
+
+
 func _ready():
-	var idle_anim_node = get_node("IdleAnim/")
+	idle_anim_node = get_node("IdleAnim/")
+	collision_box_node = get_node("CollisionShape2D")
 	idle_anim_node.play()
+	
+
+func _process(delta):
+	if collision_box_node.get_colliders():
+		print("oof ow ouch.")
