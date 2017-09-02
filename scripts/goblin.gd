763bde3eb30ffc05ec0bec313d0956bfbf80a065
Waluigi <walomator@protonmail.com>
2017-09-02 19:03:16 -0400

    dev: Allow each monster's scripts to inherit from a generic monster script

---
+++ scripts/goblin.gd
@@ -0,0 +1,13 @@
+extends "monster.gd"
+
+
+const BOUNCINESS = 200
+
+
+func _ready():
+	pass
+	
+
+func be_a_slime():
+	print("Yes, I'm a slime!")
+	
--- scripts/monster.gd
+++ scripts/monster.gd
@@ -3,14 +3,32 @@ extends KinematicBody2D
 
 var idle_anim_node
 var collision_box_node
+var monster_type
+
+#var Monster
 
 
 func _ready():
+	set_fixed_process(true)
+	monster_type = get_name()
+	# Stop what you're doing and use subclasses instead
+#	_set_monster_class(monster_type)
+	
 	idle_anim_node = get_node("IdleAnim/")
 	collision_box_node = get_node("CollisionShape2D")
 	idle_anim_node.play()
 	
 
-func _process(delta):
-	if collision_box_node.get_colliders():
-		print("oof ow ouch.")
+func _fixed_process(delta):
+	if get_collider():
+		print("Oof ow ouch.")
+	
+
+func _set_monster_class(type):
+#	if type == "Slime":
+#		Monster = load("res://scripts/slime.gd")
+#	elif type == "Goblin":
+#		Monster = load("res://scripts/goblin.gd")
+#	else:
+#		pass # Should have a fallback to prevent crashes
+	pass
