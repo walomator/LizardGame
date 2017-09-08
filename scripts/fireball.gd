8c79ea418097399d6acc4da112ce1070a4bdfc09
Waluigi <walomator@protonmail.com>
2017-09-08 11:01:33 -0400

    dev: Make fireball.gd inhereit from more generic particle.gd

--- scripts/fireball.gd
+++ scripts/fireball.gd
@@ -1,14 +1,4 @@
-extends KinematicBody2D
-
-# Should extend a particle script instead
-# Should create a simple_timer class for use here and with collision_handler
-
-var spawner
-var direction = 0
-#var lifespan = 0
-
-var SimpleTimer = load("res://scripts/simple_timer.gd")
-
+extends "particle.gd"
 
 # Times overlap - ie fireball despawns 4 seconds after creation
 const START_VELOCITY = 200
@@ -16,54 +6,11 @@ const TIME_TO_START_FLICKER = 1
 const FLICKER_INTERVAL = 0.05
 const TIME_TO_DIE = 1.4
 
-onready var sprite_node = self.get_node("Sprite")
-
 
 func _ready():
 	set_fixed_process(true)
 	start_timer("TIME_TO_START_FLICKER", TIME_TO_START_FLICKER)
 	start_timer("TIME_TO_DIE", TIME_TO_DIE)
-	
-
-func _fixed_process(delta):
-	if direction:
-		move(Vector2(direction * START_VELOCITY * delta, 0))
-	
-
-func set_direction(object_direction):
-	print(object_direction)
-	if object_direction == "right":
-		direction = 1
-	elif object_direction == "left":
-		direction = -1
-	elif object_direction == "still":
-		direction = 0
-	
-
-func set_spawner(object_spawner):
-	spawner = object_spawner
-	
-
-func start_timer(name, time):
-	var simple_timer = SimpleTimer.new()
-	simple_timer.start(self, name, time)
-	
-#	var timer = Timer.new()
-#	timer.connect("timeout", self, "handle_timeout")
-#	self.add_child(timer)
-#	timer.set_wait_time(time)
-#	timer.set_one_shot(true)
-#	timer.start()
-
-func flicker_switch():
-#	sprite_node.set_hidden(true)
-	sprite_node.set_hidden(not sprite_node.is_hidden())
-	
-
-func die():
-	for child in self.get_children():
-		child.queue_free()
-	self.queue_free()
 	
 
 func handle_timeout(timer_object, name):
---
+++ scripts/particle.gd
@@ -0,0 +1,56 @@
+extends KinematicBody2D
+
+var spawner
+var direction = 0
+
+var SimpleTimer = load("res://scripts/simple_timer.gd")
+
+
+# Times overlap
+const START_VELOCITY = 0
+const TIME_TO_START_FLICKER = 0
+const FLICKER_INTERVAL = 0
+const TIME_TO_DIE = 0
+
+onready var sprite_node = self.get_node("Sprite")
+
+
+func _ready():
+	# Need to set fixed_process to true in subclasses
+	pass
+	
+
+func _fixed_process(delta): # FEAT - Should be more dynamic
+	if direction:
+		move(Vector2(direction * START_VELOCITY * delta, 0))
+	
+
+func set_direction(object_direction):
+	if object_direction == "right":
+		direction = 1
+	elif object_direction == "left":
+		direction = -1
+	elif object_direction == "still":
+		direction = 0
+	
+
+func set_spawner(object_spawner):
+	spawner = object_spawner
+	
+
+func start_timer(name, time):
+	var simple_timer = SimpleTimer.new()
+	simple_timer.start(self, name, time)
+	
+
+func flicker_switch():
+	sprite_node.set_hidden(not sprite_node.is_hidden())
+	
+
+func die():
+	self.queue_free()
+	
+
+func handle_timeout(timer_object, name):
+	# Should be overloaded by subclass
+	timer_object.queue_free()
